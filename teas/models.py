198cf473036b5bfb8e932d4d296469d5b117af5f
Jakub Biakowski <bialkowski.kuba@gmail.com>
2023-09-02 22:43:20 +0200

    Created requests for getting details and filtering

--- teas/models.py
+++ teas/models.py
@@ -9,23 +9,54 @@ class Tea(models.Model):
         BLACK = 'BLACK', _('Czarna')
         RED = 'RED', _('Czerwona')
 
-    tea_name = models.CharField(max_length=100)
+    tea_name = models.CharField(max_length=100, default='')
     tea_type = models.CharField(choices=TeaTypes.choices, max_length=10)
 
     def __str__(self):
-        return self.tea_type
+        return '{ id: ' + str(self.pk) + ', name: ' + self.tea_name + ' }'
+
+    def serialize(self):
+        return {
+            "id": self.pk,
+            "teaName": self.tea_name,
+            "teaType": dict(self.TeaTypes.choices).get(self.tea_type)
+        }
 
 
 class TeaDescription(models.Model):
-    tea = models.ForeignKey(Tea, on_delete=models.CASCADE)
+    tea_ref = models.ForeignKey(Tea, on_delete=models.CASCADE)
+    order = models.IntegerField(default=0)
     heading = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
 
+    def __str__(self):
+        return '{ teaRef: ' + str(getattr(self.tea_ref, 'pk')) + ', order: ' + str(self.order) + ', heading: ' + self.heading[0:20] + ' [...], description: ' + self.description[0:20] + ' [...] }'
+
+    def serialize(self):
+        return {
+            'order': self.order,
+            'header': self.heading,
+            'description': self.description
+        }
+
 
 class TeaCharacteristics(models.Model):
-    tea = models.OneToOneField(Tea, on_delete=models.CASCADE)
+    tea_ref = models.OneToOneField(Tea, on_delete=models.CASCADE)
     tea_amount = models.CharField(max_length=20)
     water_temp = models.IntegerField()
-    steeping_time = models.IntegerField(verbose_name='Steeping time in minutes')
+    steeping_time = models.IntegerField(
+        verbose_name='Steeping time in minutes')
     amount_of_steeps = models.IntegerField()
     notes = models.CharField(max_length=100)
+
+    def __str__(self):
+        return '{ teaRef: ' + str(getattr(self.tea_ref, 'pk')) + ' }'
+
+    def serialize(self):
+        return {
+            'teaAmount': self.tea_amount,
+            'waterTemp': self.water_temp,
+            'steepingTime': self.steeping_time,
+            'amountOfSteeps': self.amount_of_steeps,
+            'notes': self.notes
+        }
--- teas/views.py
+++ teas/views.py
@@ -1,12 +1,50 @@
-from django.http import JsonResponse
-
-from .models import Tea
-
-
-def index(request):
-    return JsonResponse({'test': 'Henlo it is tea time :D'})
+from django.http import HttpRequest, JsonResponse
+from django.shortcuts import get_object_or_404
+from .models import Tea, TeaDescription, TeaCharacteristics
 
 
-def details(request, tea_id):
-    black = list(Tea.objects.filter(tea_type='BLACK'))
-    return JsonResponse({'test': 'Henlo it is temst :D %s' % black[0]})
+def details(request: HttpRequest, tea_id: int):
+    if request.method != 'GET':
+        return JsonResponse({'errorInfo': 'Method Not Allowed'}, status=405)
+
+    foundTea = get_object_or_404(Tea, pk=tea_id)
+
+    try:
+        foundDetails = TeaDescription.objects.filter(tea_ref=tea_id)
+        if len(foundDetails) == 0:
+            raise TeaDescription.DoesNotExist
+    except TeaDescription.DoesNotExist:
+        return JsonResponse({'errorInfo': 'no_description'}, status=500)
+
+    try:
+        foundCharacteristics = TeaCharacteristics.objects.get(tea_ref=tea_id)
+    except TeaCharacteristics.DoesNotExist:
+        return JsonResponse({'errorInfo': 'no_characteristics'}, status=500)
+
+    return JsonResponse(
+        {'tea': foundTea.serialize(),
+         'description': [detail.serialize() for detail in foundDetails],
+         'characteristics': foundCharacteristics.serialize()
+         }, status=200)
+
+
+def filter(request: HttpRequest):
+    if request.method != 'GET':
+        return JsonResponse({'errorInfo': 'Method Not Allowed'}, status=405)
+
+    tea_types = request.GET.getlist('type', [])
+    search = request.GET.get('search', '')
+
+    print(tea_types)
+    print(search)
+
+    if len(tea_types) == 0 and len(search) == 0:
+        return JsonResponse([tea.serialize() for tea in Tea.objects.all()], safe=False)
+
+    if len(tea_types) == 0:
+        return JsonResponse([tea.serialize() for tea in Tea.objects.filter(tea_name__icontains=search)], safe=False)
+
+    if len(search) == 0:
+        return JsonResponse([tea.serialize() for tea in Tea.objects.filter(tea_type__in=tea_types)], safe=False)
+
+    return JsonResponse([tea.serialize() for tea in Tea.objects.filter(tea_type__in=tea_types, tea_name__icontains=search)], safe=False)
