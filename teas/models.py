1db4a9b0ac102daa1396a50cc0c9d110c492208b
Jakub Biakowski <bialkowski.kuba@gmail.com>
2023-09-02 08:06:21 +0200

    Created models and testing urls

--- teas/models.py
+++ teas/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Tea(models.Model):
+    class TeaTypes(models.TextChoices):
+        GREEN = 'GREEN', _('Zielona')
+        WHITE = 'WHITE', _('Biała')
+        BLACK = 'BLACK', _('Czarna')
+        RED = 'RED', _('Czerwona')
+
+    tea_name = models.CharField(max_length=100)
+    tea_type = models.CharField(choices=TeaTypes.choices, max_length=10)
+
+    def __str__(self):
+        return self.tea_type
+
+
+class TeaDescription(models.Model):
+    tea = models.ForeignKey(Tea, on_delete=models.CASCADE)
+    heading = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+
+
+class TeaCharacteristics(models.Model):
+    tea = models.OneToOneField(Tea, on_delete=models.CASCADE)
+    tea_amount = models.CharField(max_length=20)
+    water_temp = models.IntegerField()
+    steeping_time = models.IntegerField(verbose_name='Steeping time in minutes')
+    amount_of_steeps = models.IntegerField()
+    notes = models.CharField(max_length=100)
--- teas/views.py
+++ teas/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from .models import Tea
+
+
+def index(request):
+    return JsonResponse({'test': 'Henlo it is tea time :D'})
+
+
+def details(request, tea_id):
+    black = list(Tea.objects.filter(tea_type='BLACK'))
+    return JsonResponse({'test': 'Henlo it is temst :D %s' % black[0]})
